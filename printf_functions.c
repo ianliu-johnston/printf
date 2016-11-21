55aa9064576f0fea21672679680b60e37eeaa27c
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-21 13:54:43 -0800

    changed types to char *

--- printf_functions.c
+++ printf_functions.c
@@ -1,34 +1,31 @@
 #include <stdarg.h>
 #include "holberton.h"
-void found_char(va_list c)
+char *found_char(va_list c)
 {
-	_putchar(va_arg(c, int));
+	return (va_arg(c, int));
 }
-void found_string(va_list *s)
+char *found_string(va_list *s)
 {
 	char *string;
-	int j;
 
 	string = va_arg(s, char *);
-	if (string)
-		for (j = 0; string[j] != '\0'; j++)
-			_putchar(string[j]);
+	return (string);
 }
-void found_int(va_list i)
+char *found_int(va_list i)
 {
 	int toint;
 	int n;
 
 	toint = va_arg(i, char *);
-	for (n = 0; n > toint; n /= 10)
+	for (n = 0; n < ; n /= 10)
 		_putchar(toint);
 
 }
-void found_float(va_list flo)
+char *found_float(va_list flo)
 {
 	_putchar(va_arg(flo, double));
 }
-void found_percent(va_list percentsign)
+char *found_percent(va_list percentsign)
 {
 	_putchar(va_arg(percentsign, int));
 }
