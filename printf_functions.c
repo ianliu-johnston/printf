ee5528d63ab9daac19ae5c48fdc38e0eb9598f12
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-21 14:22:39 -0800

    fixed returns to be char *

--- printf_functions.c
+++ printf_functions.c
@@ -2,7 +2,10 @@
 #include "holberton.h"
 char *found_char(va_list c)
 {
-	return (va_arg(c, int));
+	char *string;
+
+	string = (va_arg(c, int));
+	return (string);
 }
 char *found_string(va_list *s)
 {
@@ -13,14 +16,22 @@ char *found_string(va_list *s)
 }
 char *found_int(va_list i)
 {
-	return (va_arg(i, int));
+	char *string;
 
+	string = (va_arg(i, int));
+	return (string);
 }
 char *found_float(va_list flo)
 {
-	return (va_arg(flo, int));
+	char *string;
+
+	string = (va_arg(flo, double));
+	return (string);
 }
 char *found_percent(va_list percentsign)
 {
-	return (va_arg(percentsign, int));
+	char *string;
+
+	string = (va_arg(percentsign, double));
+	return (string);
 }
