6408621308ae1fd74db4df3e94febfb12423f925
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-21 20:34:02 -0800

    Fixed documentation, updated gitignore

--- printf_functions.c
+++ printf_functions.c
@@ -1,5 +1,10 @@
 #include <stdarg.h>
 #include "holberton.h"
+/**
+  * found_char - converts a char to a string
+  * @c: char to convert
+  * Return: 2 byte string
+  */
 char *found_char(va_list c)
 {
 	char string[2];
@@ -10,6 +15,11 @@ char *found_char(va_list c)
 	string[1] = '\0';
 	return (p);
 }
+/**
+  * found_string - passes a string through
+  * @s: string to pass through
+  * Return: unmodified string
+  */
 char *found_string(va_list *s)
 {
 	char *string;
