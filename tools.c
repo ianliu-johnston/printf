7f2f8011d6acbc4e81ae22e452ae1c953e207eba
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-11-23 13:13:10 -0800

    Took out unneeded atoi function

--- tools.c
+++ tools.c
@@ -1,34 +1,4 @@
 #include <unistd.h>
-
-/**
- * _atoi - converts a string to an integer.
- * @s: the string to convert
- * Return: the converted string.
- */
-int _atoi(char *s)
-{
-	short boolean;
-	int i, minus, result;
-
-	result = boolean = 0;
-	minus = -1;
-
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] == '-')
-			minus *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result -= (s[i] - '0');
-			boolean = 1;
-		}
-		else if (boolean == 1)
-			break;
-	}
-	return (result * minus);
-}
 /**
  * _memcpy - copies memory area
  * @dest: pointer to destination memory area
